170a04cc2b79555fda2c64536cbd969907793ed2
shimmerVoltage <shimmervoltage@yandex.ru>
2026-03-25 17:19:31 +0300

    CreamAmination method in progress

--- Assets/Scripts/ItemDragHandler.cs
+++ Assets/Scripts/ItemDragHandler.cs
@@ -1,24 +1,30 @@
 using System.Collections;
+using System.Xml.Linq;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class ItemDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
 	CanvasGroup canvasGroup;
 
+	[SerializeField] private byte creamMoves;
 	[SerializeField] private float acneRemoveDuration;
-	[SerializeField] private Transform faceTrigger;
+	[SerializeField] private float randomPositionOffset;
 	[SerializeField] private Image acneImage;
+	[SerializeField] private RectTransform faceTrigger;
 
 	private string faceTriggerName;
 	private Vector2 startPosition;
+	//private RectTransform faceTriggerRectTransform;
 
 	private void Start()
 	{
 		faceTriggerName = faceTrigger.name;
 		startPosition = transform.position;
 		canvasGroup = GetComponent<CanvasGroup>();
+		
 	}
 
 	public void OnBeginDrag(PointerEventData eventData)
@@ -40,12 +46,13 @@ public class ItemDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, I
 		//Debug.Log("OnEndDrag");
 		//Debug.Log("eventData.pointerEnter?.GetComponent<Transform>().name = " + eventData.pointerEnter?.GetComponent<Transform>().name);
 
-		if (eventData.pointerEnter?.GetComponent<Transform>().name == faceTriggerName)
+		if (eventData.pointerEnter?.GetComponent<UnityEngine.Transform>().name == faceTriggerName)
 		{
 			Debug.Log("true");
 			//acneObject.GetComponent<Image>().color = Color.blue;
 			//acneImage.color = Color.blue;
 			StartCoroutine(AcneFadeOut());
+			StartCoroutine(CreamAmination());
 		}
 		else
 		{
@@ -72,6 +79,48 @@ public class ItemDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, I
 			);
 
 			yield return null;
-		}				
+		}
+	}
+
+	private IEnumerator CreamAmination()
+	{
+		for (int i = 0; i <= creamMoves; i++)
+		{
+			float elapsed = 0.0f;
+
+			Vector2 startPosition = transform.position;
+			Vector2 targetPosition = Vector2.zero;
+
+			if (i != creamMoves)
+			{
+				targetPosition = new Vector2
+					(
+						Random.Range
+							(
+								faceTrigger.position.x - faceTrigger.rect.width / 2,
+								faceTrigger.position.x + faceTrigger.rect.width / 2
+							),
+						Random.Range
+							(
+								faceTrigger.position.y - faceTrigger.rect.height / 2,
+								faceTrigger.position.y + faceTrigger.rect.height / 2
+							)
+					);
+			}
+			else
+			{
+				targetPosition = this.startPosition;
+			}
+
+			while (elapsed < (acneRemoveDuration / creamMoves))
+			{
+				elapsed += Time.deltaTime;
+				float time = elapsed / (acneRemoveDuration / creamMoves);
+				Vector2 newPosition = Vector2.Lerp(startPosition, targetPosition, time);
+				transform.position = newPosition;
+
+				yield return null;
+			}
+		}
 	}
 }
